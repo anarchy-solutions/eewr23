f30afdf48b365aaa1cb9557ec64e4c3c98566724
anarchy-solutions <164175212+anarchy-solutions@users.noreply.github.com>
2025-10-25 02:58:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ const minecraftFont = Press_Start_2P({
 })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Nico...",
+  description: "Te amo, Nico...",
+  generator: "surge.xyz",
 }
 
 export default function RootLayout({
